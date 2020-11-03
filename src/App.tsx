da582e3fec7324e337551278e19753bf9a8ee91f
Christopher DiStasio <cadistasio@gmail.com>
2020-11-03 17:18:52 -0600

    stubs out home, navbar, layout app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+  
+import React, {Component} from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/main/Navbar';
+import HomePage from './components/main/HomePage';
+import FoodAdvisorsContainer from '../containers/FoodAdvisorsContainer'
+import TrackerHome from './trackers/TrackerHome'
+import JournalHome from './journal/JournalHome'
+import JournalContainer from '../containers/JournalContainer'
+import ResourceHome from './resources/ResourceHome'
+import ResourcesContainer from '../containers/ResourcesContainer'
+import CommunityHome from './community/CommunityHome'
+import BottomNavbar from './BottomNavbar'
+import { Layout } from './components/main/Layout'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <>
+        <TopNavbar />
+        <Layout>
+          <Switch>
+            {
+              <Route 
+                exact path='/' 
+                component={Advisor}
+              />
+            }
+            {
+              <Route 
+                exact path='/tracker_home' 
+                component={TrackerHome}
+              />
+            }
+            {
+              <Route 
+                exact path='/journal_home' 
+                component={JournalHome}
+              />
+            }
+            {
+              <Route 
+                exact path='/resource_home' 
+                component={ResourceHome}
+              />
+            }
+            {
+              <Route 
+                exact path='/community_home' 
+                component={CommunityHome}
+              />
+            }
+          </Switch>
+
+          <FoodAdvisorsContainer />
+          
+          <JournalContainer />
+          
+          <ResourcesContainer />
+
+        </Layout>
+        <BottomNavbar />
+      </>
+    )
+  }
 }
-
-export default App;
+export default App

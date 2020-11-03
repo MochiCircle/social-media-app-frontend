6c839954f3d9e97ecd5c4c86c37cf20d087b6be1
mcdonalddylan <soupaface@gmail.com>
2020-11-03 00:38:59 -0500

    added react-strap, react types, and very incomplete components

---
+++ src/components/NavBarComp.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface IProps{
+
+}
+
+const LoginComp: React.FC<IProps> = (props:IProps) => 
+{
+    return (
+    <div className="container">
+        
+    </div>
+    )
+}

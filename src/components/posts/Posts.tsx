3e44c6a2d1c5ab9a4374b8a62d7319d9d6de58e3
Tucker Fritz <tuckerfritz15@gmail.com>
2020-11-04 16:59:30 -0600

    Started on posts feature

---
+++ src/components/posts/Posts.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface IProps {
+    postId:number,
+    postText:string,
+    likes:number
+}
+
+export const Post:React.FC<IProps> = (props:IProps) => {
+    return (
+        <div>
+
+        </div>
+    )
+}

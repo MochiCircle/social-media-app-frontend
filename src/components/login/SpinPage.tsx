eafcd87ac016e884e125e4c1450505bc46f8e214
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2020-11-12 12:53:10 -0800

    auto redirects from spin

--- src/components/login/SpinPage.tsx
+++ src/components/login/SpinPage.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { userCorrected } from "../../util/Models";
 
-const SpinPage = () =>
-{
-    return(
-        <div className="mochi-guy">
-            <h3>Look at him go!</h3>
-        </div>
-    )
-}
+const SpinPage: React.FC<userCorrected> = (props: userCorrected) => {
+  if (props.username) {
+    window.location.href = "/";
+  }
 
-export default SpinPage;
+  return (
+    <div className="mochi-guy">
+      <h3>Look at him go!</h3>
+    </div>
+  );
+};
+
+const mapStateToProps = (appState: any) => {
+  return {
+    id: appState.loginState.id,
+    username: appState.loginState.username,
+    password: appState.loginState.password,
+    firstname: appState.loginState.firstname,
+    lastname: appState.loginState.lastname,
+    email: appState.loginState.email,
+    picUrl: appState.loginState.picUrl,
+    status: appState.loginState.status,
+    bio: appState.loginState.bio,
+    interests: appState.loginState.interests,
+    verified: appState.loginState.verified,
+  };
+};
+
+export default connect<userCorrected>(mapStateToProps)(SpinPage);
